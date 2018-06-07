de5ad77be78ee34860916f7b7550aa9df9784628
Alexandre <alexandre.alex10@hotmail.fr>
2018-06-07 13:54:38 +0200

    Add files via upload

--- read_instruction.py
+++ read_instruction.py
@@ -1,58 +1,9 @@
 import converter
 
-def check_and_erase_neighbours(tab, x, y):
-
-    def rec(tab, x, y, x_parent, y_parent):
-
-        neighbours = []
-
-        xleft = (x == 0)
-        xright = (x == len(tab) - 1)
-        yup = (y == 0)
-        ybottom = (y == len(tab[0]) - 1)
-
-        # On sauvegarde la valeur de la case
-        val = tab[y][x]
-        # On détruit la case
-        tab[y][x] = -1
-
-        # On regarde si les voisins sont de meme valeur et ne sont pas des cases déjà testées
-
-        if not xleft and x - 1 != x_parent:
-            neighbours.append(tab[y][x - 1])
-            if tab[y][x - 1] == val:
-                tab[y][x - 1] = -1
-                # On propage la destruction au voisin
-                rec(tab, x - 1, y, x, y)
-
-        if not yup and y - 1 != y_parent:
-            neighbours.append(tab[y - 1][x])
-            if tab[y - 1][x] == val:
-                tab[y - 1][x] = -1
-                # On propage la destruction au voisin
-                rec(tab, x, y - 1, x, y)
-
-        if not xright and x + 1 != x_parent:
-            neighbours.append(tab[y][x + 1])
-            if tab[y][x + 1] == val:
-                tab[y][x + 1] = -1
-                # On propage la destruction au voisin
-                rec(tab, x + 1, y, x, y)
-
-        if not ybottom and y + 1 != y_parent:
-            neighbours.append(tab[y + 1][x])
-            if tab[y + 1][x] == val:
-                tab[y + 1][x] = -1
-                # On propage la destruction au voisin
-                rec(tab, x, y + 1, x, y)
-
-        return val in neighbours
-
-    return rec(tab, x, y, x, y)
-
 def has_same_value_neighbour(tab, x, y):
 
     neighbours = []
+    val = tab[y][x]
 
     xleft = (x == 0)
     xright = (x == len(tab) - 1)
@@ -60,20 +11,38 @@ def has_same_value_neighbour(tab, x, y):
     ybottom = (y == len(tab[0]) - 1)
 
     if not xleft:
-        neighbours.append(tab[y][x - 1])
-    if not yup:
-        neighbours.append(tab[y - 1][x])
-    if not xright:
-        neighbours.append(tab[y][x + 1])
-    if not ybottom:
-        neighbours.append(tab[y + 1][x])
+        if tab[y][x - 1] == val:
+            neighbours.append([x, y])
 
-    return tab[x][y] in neighbours
+    if not yup:
+        if tab[y - 1][x]] == val:
+            neighbours.append([x, y])
+
+    if not xright:
+        if tab[y][x + 1] == val:
+            neighbours.append([x, y])
+
+    if not ybottom:
+        if tab[y + 1][x] == val:
+            neighbours.append([x, y])
+
+    return neighbours
+
+def del_voisin(tab, x, y, voisin):
+    all_v = has_same_value_neighbour(tab, x, y)
+    for elem in all_v:
+        if elem not in voisin:
+            voisin.append(elem)
+            del_voisin(tab, elem[0], elem[1], voisin)
+
 
 def clic(tab, inst):
     inst = inst.split(" ")
-    if has_same_value_neighbour(tab, int(inst[0]), int(inst[1])):
-        tab[int(inst[1])][int(inst[0])] += 1
+    save = tab[int(inst[1])][int(inst[0])] + 1
+    t = del_voisin(tab, int(inst(0)), int(inst(1)), [int(inst(0)), int(inst(1))])
+    for elem in t:
+        tab[elem[1]][elem[0]] = "."
+    tab[int(inst[1])][int(inst[0])] = save
     return tab
 
 def _set(tab, inst):
