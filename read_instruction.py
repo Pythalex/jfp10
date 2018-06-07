b95a6df629d1c429517ed7d0c2ac73be931a2213
Alexandre <alexandre.alex10@hotmail.fr>
2018-06-07 11:31:42 +0200

    Add files via upload

--- read_instruction.py
+++ read_instruction.py
@@ -1,11 +1,13 @@
 def _set(tab, instruction):
     instruction = instruction.split(" ")
-    tab[instruction[1]][instruction[0]] = instruction[2]
-    return 0
+    tab[int(instruction[1])][int(instruction[0])] = int(instruction[2])
+    return tab
 
-def _set(tab, instruction):
+def _get(tab, instruction):
     instruction = instruction.split(" ")
-    return tab[instruction[1]][instruction[0]]
+    return tab[int(instruction[1])][int(instruction[0])]
+
+
 
 def read_instruction(tab, instruction):
     # clic 0 0
@@ -16,19 +18,24 @@ def read_instruction(tab, instruction):
     instruction = instruction.lower()
 
     if instruction[0] == "c":
-        return clic(tab, instruction[4:])
+        return clic(tab, instruction[5:])
 
     elif instruction[0] == "r":
-        return reserve(tab, instruction[7:])
+        return reserve(tab, instruction[8:])
 
     elif instruction[0:2] == "sc":
-        return score(tab, instruction[5:])
+        return score(tab, instruction[6:])
 
     elif instruction[0:2] == "se":
-        return _set(tab, instruction[3:])
+        return _set(tab, instruction[4:])
 
     elif instruction[0] == "g":
-        return _get(tab, instruction[3:])
+        return _get(tab, instruction[4:])
     
     else:
         return -1
+
+if __name__ == "__main__":
+    a = [[4,1,2,1,2],[1,1,3,1,3],[2,2,1,1,3],[3,4,1,3,3],[3,1,1,2,1]]
+    b = "SET 3 3 4"
+    print(read_instruction(a, b))
